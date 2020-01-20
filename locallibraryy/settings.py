097b73ea4972be9fbb62fa961b180226f9ee84d4
Руслан <https://github.com/vorbey/engin>
2020-01-20 03:22:13 +0300

    'fdff'

--- locallibraryy/settings.py
+++ locallibraryy/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['<your app URL without the https:// prefix>.herokuapp.com','127.0.0.1']
 
 
 # Application definition
